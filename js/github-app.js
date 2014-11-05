60cf76719fbc48a48f8cf75a0a4e8a99c0febafc
Daniel Huie <huied93@uw.edu>
2014-11-05 12:24:02 -0800

    end of class

--- js/github-app.js
+++ js/github-app.js
@@ -8,11 +8,23 @@
 
 
 angular.module('GitHubApp', [])
-    .controller('GitHubController', function($scope) {
+    .controller('GitHubController', function($scope, $http) {
         $scope.userName = 'drstearns';
         $scope.getRepos = function() {
 
-            //add code here
+            $scope.loading = true;
+            $http.get('https://api.github.com/users/' + $scope.userName + '/repos')
+                .success(function(data) {
+                    $scope.repos = data;
+                    $scope.errorMessage = null;
+                })
+                .error(function(err) {
+                    // errorMessage var in html will only if we enter this error function
+                    $scope.errorMessage = err.message || 'user does not exist';
+                })
+                .finally(function() { // called when with success or error is called, applies DRY principles
+                    $scope.loading = false;
+                });
 
         };
     });
